fc2a736648d14b26a2201b2597190bc5bbb73c6a
Riyando433 <sepfriyan.p@students.amikom.ac.id>
2021-01-17 21:20:07 +0700

    UAS

---
+++ app/src/main/java/id/sepfriyanp/githubuser/api/Api.kt
@@ -0,0 +1,32 @@
+package id.sepfriyanp.githubuser.api
+
+import id.sepfriyanp.githubuser.data.model.DetailUserResponse
+import id.sepfriyanp.githubuser.data.model.User
+import id.sepfriyanp.githubuser.data.model.UserResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface Api {
+    @GET("search/users")
+    fun getSearchUsers(
+        @Query("q") query: String
+    ): Call<UserResponse>
+
+    @GET("users/{username}")
+    fun getUserDetail(
+        @Path("username") username : String
+    ): Call<DetailUserResponse>
+
+    @GET("users/{username}/followers")
+    fun getFollowers(
+        @Path("username") username: String
+    ): Call<ArrayList<User>>
+
+    @GET("users/{username}/following")
+    fun getFollowing(
+        @Path("username") username: String
+    ): Call<ArrayList<User>>
+}
---
+++ app/src/main/java/id/sepfriyanp/githubuser/data/model/UserResponse.kt
@@ -0,0 +1,5 @@
+package id.sepfriyanp.githubuser.data.model
+
+data class UserResponse(
+    val items : ArrayList<User>
+)
